e1855e9e1b528e144baa6b2c33db58fc6a88fd7f
Mark Shavlik <mark@senserva.com>
2025-05-22 19:31:48 -0500

    Revert "save"
    This reverts commit fa704c0b4973218eb957bd650b8df8eff34a74c4.

--- Chefs/Platforms/WebAssembly/Program.cs
+++ Chefs/Platforms/WebAssembly/Program.cs
@@ -4,16 +4,29 @@ namespace Simeserva;
 
 public class Program
 {
-	static async Task Main(string[] args)
+#if IS_WASM_SKIA
+		static async Task Main(string[] args)
+#else
+		static int Main(string[] args)
+#endif
+		{
+			App.InitializeLogging();
 
-	{
-		App.InitializeLogging();
+#if !IS_WASM_SKIA
+			Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/MaterialIcons-Regular.ttf#Material Symbols Outlined");
+			Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/FontAwesome-Brands.otf#Font Awesome 6 Brands");
+#endif
 
-		var host = UnoPlatformHostBuilder.Create()
-			.App(() => new App())
-			.UseWebAssembly()
-			.Build();
+#if IS_WASM_SKIA
+			var host = UnoPlatformHostBuilder.Create()
+				.App(() => new App())
+				.UseWebAssembly()
+				.Build();
 
-		await host.RunAsync();
-	}
+			await host.RunAsync();
+#else
+			Microsoft.UI.Xaml.Application.Start(_ => new App());
+			return 0;
+#endif
+		}
 }
